ebb37f8a10196297e48b3565c7c819a75193667b
Polina Kaida <remwxx@gmail.com>
2025-10-15 12:48:40 +0100

    Complete variables task

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+//1
+let a = 7;
+let b = 4;
+console.log(a * b);
+
+//2
+let c = 15;
+let d = 5;
+console.log(c / d);
+
+//3
+let e = 8;
+let f = 11;
+console.log(e + f);
+
+//4
+const numValue = 11;
+const booleanValue = true;
+const stringValue = 'java script';
+const stringValue2 = '100';
+console.log(numValue, booleanValue, stringValue, stringValue2);
+
+//5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num ++;
+num--;
+console.log(num);
