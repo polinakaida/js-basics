fb2dc99e6393540afbe8ae34868d6e6995125051
Polina Kaida <remwxx@gmail.com>
2025-11-03 21:59:49 +0000

    Complete all tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
 //1
-let a = 7;
-let b = 4;
+const a = 7;
+const b = 4;
 console.log(a * b);
 
 //2
-let c = 15;
-let d = 5;
+const c = 15;
+const d = 5;
 console.log(c / d);
 
 //3
-let e = 8;
-let f = 11;
+const e = 8;
+const f = 11;
 console.log(e + f);
 
 //4
@@ -29,3 +29,27 @@ num /= 11;
 num ++;
 num--;
 console.log(num);
+
+//1
+const number = Number(prompt('Enter a number:'));
+const square = number ** 2;
+console.log('Square of a number: ', square);
+
+//2
+const num1 = +prompt('Enter first number:');
+const num2 = +prompt('Enter second number:');
+const average = (num1 + num2) / 2;
+console.log('Average: ', average);
+
+//3 
+const minutes = +prompt('Enter the number of minutes:');
+const seconds = minutes * 60;
+console.log(minutes + ' minutes = ', seconds, ' seconds');
+
+//4
+const greeting = 'Hello, ';
+const userName = prompt('Enter yout name:');
+const message = greeting + userName + '!';
+alert(message);
+
+document.write("<p>" + message + "</p>");
